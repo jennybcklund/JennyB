31e20a6588a67da551b44f7c576f67b339c71634
jennybacklund <33482998+jennybacklund@users.noreply.github.com>
2018-04-04 14:06:56 +0200

    Removed dublicate view "films_in_genre"

--- database.sql
+++ database.sql
@@ -352,19 +352,6 @@ INSERT INTO `rentaldetails` VALUES (1,1,2,NULL),(2,11,3,NULL),(3,12,4,NULL),(4,1
 UNLOCK TABLES;
 
 --
--- Temporary view structure for view `show_film_in_genre`
---
-
-DROP TABLE IF EXISTS `show_film_in_genre`;
-/*!50001 DROP VIEW IF EXISTS `show_film_in_genre`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `show_film_in_genre` AS SELECT 
- 1 AS `Genre`,
- 1 AS `Film`*/;
-SET character_set_client = @saved_cs_client;
-
---
 -- Final view structure for view `films_in_genre`
 --
 
@@ -399,24 +386,6 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `show_film_in_genre`
---
-
-/*!50001 DROP VIEW IF EXISTS `show_film_in_genre`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `show_film_in_genre` AS select `g`.`genreType` AS `Genre`,group_concat(distinct `f`.`title` separator ', ') AS `Film` from ((`film_has_genre` `fg` left join `genre` `g` on((`g`.`idGenre` = `fg`.`idGenre`))) left join `film` `f` on((`f`.`idFilm` = `fg`.`idFilm`))) group by `g`.`genreType` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -427,4 +396,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-04 13:51:02
+-- Dump completed on 2018-04-04 14:06:18
