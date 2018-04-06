ed40d95b94a34791fc2748bc09b3932e4013c5c9
jennybacklund <33482998+jennybacklund@users.noreply.github.com>
2018-04-06 14:05:10 +0200

    Added function to check if film is overdue or not

--- database.sql
+++ database.sql
@@ -556,4 +556,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-06 11:34:15
+-- Dump completed on 2018-04-06 14:03:50
--- queries.sql
+++ queries.sql
@@ -29,7 +29,8 @@ SELECT @var_rentDate, @var_dueDate, @var_idRental, @var_returnDate, @var_message
 
 -- Fråga 8: Gör en funktion som tar en film som parameter och returnerar olika värden beroende på om filmen är sent inlämnad eller inte. Dvs, om du matar in film nr 345 ska du få tillbaka TRUE om filmen är uthyrd men borde vara tillbakalämnad, annars FALSE. (1 och 0 funkar också om det är lättare.)
 
-SELECT * FROM products;
+SELECT f_check_if_past_due_date(170); -- True - film is rented and overdue
+SELECT f_check_if_past_due_date(166); -- False - film is not overdue/or rented
 
 -- Fråga 9: En Stored Procedure som ska köras när en film lämnas tillbaka. Den ska använda sig av ovanstående funktion för att göra någon form av markering/utskrift om filmen är återlämnad för sent.
 
